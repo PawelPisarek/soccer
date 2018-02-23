1de283d01643113f7797ee2ae1e481d412c8fb17
Unknown <rybacka.ola@gmail.com>
2018-02-23 10:43:09 +0100

    Field

---
+++ src/Field/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class Field extends Component {
+  render() {
+    return (
+      <div className="field">
+      </div>
+    )
+  }
+}
+
+export default Field
